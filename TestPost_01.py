6ca407fdf7c0967fd432c2605a2161e57384e341
edveralli <everalli@buenosaires.gob.ar>
2021-11-01 22:48:16 -0300

    testPost

--- TestPost_01.py
+++ TestPost_01.py
@@ -1,35 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 31 20:45:28 2021
-
-@author: Palmaria
-"""
-
-"""
-import requests
-
-url = 'https://www.w3schools.com/python/demopage.php'
-myobj = {'somekey': 'somevalue'}
-
-x = requests.post(url, data = myobj)
-
-print(x.text)
-
-"""
 
 import requests
 import json
 
 r = requests.post(
     "http://localhost:4000/Ciclope.svc/Find",
-    data=json.dumps({"usuario": "pepe","coleccion": "Decretos","anio": 2011,"passwd": "sesamo"}),
+    data=json.dumps({"usuario": "pepe","coleccion": "Decretos","anio": 2011,"passwd": "sesamo","fields": "title_orig,numero"}),
     headers={"Content-Type": "application/json"},
 )
 
 print(r.text)
 
-"""
-curl  -X POST http://localhost:4000/Ciclope.svc/Find -H 'Content-Type: application/json' -d'
-{"usuario": "pepe","coleccion": "Decretos","anio": 2011,"passwd": "sesamo"}'
-
-"""
